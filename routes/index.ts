53f75d8836ab0ff1db4093af8d7d4ccc52495ccd
Satorki <sebastian.kedzier@gmail.com>
2024-08-23 11:39:24 +0200

    Styles for index page added

--- routes/index.ts
+++ routes/index.ts
@@ -2,7 +2,11 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.render("index", { title: "Main Page", logged: !!req.session.user });
+  res.render("index", {
+    title: "Main Page",
+    user: req.session.user?.nickName,
+    logged: !!req.session.user,
+  });
 });
 
 export default router;
