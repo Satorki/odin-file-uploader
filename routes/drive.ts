b9178a36ae2848f09f40929fe1c5930f304e4ed7
Satorki <sebastian.kedzier@gmail.com>
2024-08-23 12:00:55 +0200

    Created a view for new file

--- routes/drive.ts
+++ routes/drive.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
+import { PrismaClient, User } from "@prisma/client";
+
 const router = express.Router();
-import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 router.get("/drive", async (req: Request, res: Response) => {
@@ -9,7 +10,38 @@ router.get("/drive", async (req: Request, res: Response) => {
       createdAt: "desc",
     },
   });
-  res.render("drive", { title: "Drive", files });
+
+
+  const user = req.session.user as User | undefined;
+
+  res.render("drive", {
+    title: "Drive",
+    files,
+    user: user?.nickName,
+    logged: !!user,
+  });
+});
+
+router.get("/drive/new-file", (req: Request, res: Response) => {
+  res.render("new-file", { title: "New File" });
+});
+
+router.post("/drive/new-file", async (req: Request, res: Response) => {
+  const user = req.session.user as User | undefined;
+
+  if (!user) {
+    return res.redirect("/log-in");
+  }
+
+  const file = await prisma.file.create({
+    data: {
+      userId: user.id, // Używaj id użytkownika
+      fileName: req.body.name,
+      filePath: req.body.path,
+    },
+  });
+
+  res.redirect("/drive");
 });
 
 export default router;
